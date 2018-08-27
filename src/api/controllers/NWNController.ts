09f946f07ef1d55172e27c867663755cecbc0609
Riari <3583774+Riari@users.noreply.github.com>
2018-08-27 22:27:39 +0100

    Deployment test (part 2)

--- src/api/controllers/NWNController.ts
+++ src/api/controllers/NWNController.ts
@@ -17,7 +17,6 @@ class NWNController extends Controller {
 
       response.on('end', () => {
         const status = JSON.parse(data)
-        status.current_players = 10
         res.send(status)
       })
     }).on('error', error => res.status(503).send({ message: 'request_failed' }))
