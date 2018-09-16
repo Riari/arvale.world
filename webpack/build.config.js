f6f6bc6a04778e6ad6c8e33483b5af6b2f235ab2
Riari <3583774+Riari@users.noreply.github.com>
2018-09-16 10:16:04 +0100

    And that

--- webpack/build.config.js
+++ webpack/build.config.js
@@ -19,6 +19,6 @@ module.exports = merge(baseConfig, {
 
 	output: {
     filename: '[name].[contenthash].js',
-		path: path.join(__dirname, '..', '..', 'dist')
+		path: path.join(__dirname, '..', 'dist')
 	}
 })
